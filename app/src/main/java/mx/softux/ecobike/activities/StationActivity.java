8591d557bfb09271b73f282f4f743e67b2c20731
Juan Villa <juan64@gmail.com>
2015-05-22 01:53:34 -0500

    Code for Specific Station Activity Title

--- app/src/main/java/mx/softux/ecobike/activities/StationActivity.java
+++ app/src/main/java/mx/softux/ecobike/activities/StationActivity.java
@@ -1,6 +1,7 @@
 package mx.softux.ecobike.activities;
 
 import android.content.Intent;
+import android.os.Bundle;
 
 import mx.softux.ecobike.P;
 import mx.softux.ecobike.R;
@@ -9,6 +10,8 @@ import mx.softux.ecobike.R;
  * Created by juan on 5/21/15.
  */
 public class StationActivity extends AbstractAppCompatActivity {
+    private int number;
+
     @Override
     protected int getContentView() {
         return R.layout.activity_station;
@@ -18,5 +21,16 @@ public class StationActivity extends AbstractAppCompatActivity {
         Intent intent = new Intent();
         intent.putExtra(P.StationActivity.NUMBER, number);
         return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getIntent().getExtras() != null && getIntent().getExtras().containsKey(P.StationActivity.NUMBER)) {
+            number = getIntent().getIntExtra(P.StationActivity.NUMBER, -1);
+            if (number != -1)
+                setTitle(getString(R.string.activity_station_title, number));
+        }
     }
 }
