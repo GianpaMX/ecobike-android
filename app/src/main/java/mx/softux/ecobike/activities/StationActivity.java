234f8cd5acaa4ab22c232e9435908c8a7b395c0d
Juan Villa <juan64@gmail.com>
2015-07-15 00:35:37 -0500

    Adding collapsible action bar

--- app/src/main/java/mx/softux/ecobike/activities/StationActivity.java
+++ app/src/main/java/mx/softux/ecobike/activities/StationActivity.java
@@ -1,7 +1,10 @@
 package mx.softux.ecobike.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import mx.softux.ecobike.P;
@@ -20,15 +23,19 @@ public class StationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station);
 
-        if (actionBarToolbar == null) {
-            actionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
-            if (actionBarToolbar != null) {
-                setSupportActionBar(actionBarToolbar);
-            }
+        actionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        if (actionBarToolbar != null) {
+            setSupportActionBar(actionBarToolbar);
         }
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+
+        RecyclerView stationListRecyclerView = (RecyclerView)findViewById(R.id.station_recycler_view);
+        stationListRecyclerView.setLayoutManager(new LinearLayoutManager(stationListRecyclerView.getContext()));
 
         station.number = getIntent().getIntExtra(P.Station.NUMBER, 0);
 
-        setTitle(getString(R.string.station_activity_title, station.number));
+        String title = getString(R.string.station_activity_title, station.number);
+        setTitle(title);
+        collapsingToolbar.setTitle(title);
     }
 }
