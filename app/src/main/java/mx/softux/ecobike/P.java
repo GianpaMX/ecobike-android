c59df95c17e879d96972c607bba5528ef4abf877
Juan Villa <juan64@gmail.com>
2015-07-14 22:18:21 -0500

    Test Station Activity Title

---
+++ app/src/main/java/mx/softux/ecobike/P.java
@@ -0,0 +1,10 @@
+package mx.softux.ecobike;
+
+/**
+ * Created by juan on 7/14/15.
+ */
+public class P {
+    public class Station {
+        public static final String NUMBER = "P.Station.NUMBER";
+    }
+}
--- app/src/test/java/mx/softux/ecobike/tests/StationTests.java
+++ app/src/test/java/mx/softux/ecobike/tests/StationTests.java
@@ -1,12 +1,18 @@
 package mx.softux.ecobike.tests;
 
+import android.content.Context;
+import android.content.Intent;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 import mx.softux.ecobike.BuildConfig;
+import mx.softux.ecobike.P;
+import mx.softux.ecobike.R;
 import mx.softux.ecobike.activities.StationActivity;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -19,9 +25,22 @@ import static org.junit.Assert.assertThat;
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(sdk=17, constants = BuildConfig.class)
 public class StationTests {
+    private static final int STATION_TEST_NUMBER = 1;
+
     @Test
     public void testActivity() {
         StationActivity stationActivity = Robolectric.setupActivity(StationActivity.class);
         assertThat(stationActivity, is(notNullValue()));
+    }
+
+    @Test
+    public void testActivityTitle() {
+        final Context context = RuntimeEnvironment.application;
+
+        Intent intent = new Intent();
+        intent.putExtra(P.Station.NUMBER, STATION_TEST_NUMBER);
+        StationActivity stationActivity = Robolectric.buildActivity(StationActivity.class).withIntent(intent).create().get();
+
+        assertThat(stationActivity.getTitle().toString(), is(context.getString(R.string.station_activity_title, STATION_TEST_NUMBER)));
     }
 }
