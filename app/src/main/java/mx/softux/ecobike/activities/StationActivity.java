299178896ae9a20585a80879f2a8e6e9f105d209
Juan Villa <juan64@gmail.com>
2015-07-14 20:47:17 -0500

    first commit of the refactor

--- app/src/main/java/mx/softux/ecobike/activities/StationActivity.java
+++ app/src/main/java/mx/softux/ecobike/activities/StationActivity.java
@@ -1,36 +1,27 @@
 package mx.softux.ecobike.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-import mx.softux.ecobike.P;
 import mx.softux.ecobike.R;
 
 /**
  * Created by juan on 5/21/15.
  */
-public class StationActivity extends AbstractAppCompatActivity {
-    private int number;
-
-    @Override
-    protected int getContentView() {
-        return R.layout.activity_station;
-    }
-
-    public static Intent newIntent(int number) {
-        Intent intent = new Intent();
-        intent.putExtra(P.StationActivity.NUMBER, number);
-        return intent;
-    }
+public class StationActivity extends AppCompatActivity {
+    private Toolbar actionBarToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_station);
 
-        if (getIntent().getExtras() != null && getIntent().getExtras().containsKey(P.StationActivity.NUMBER)) {
-            number = getIntent().getIntExtra(P.StationActivity.NUMBER, -1);
-            if (number != -1)
-                setTitle(getString(R.string.activity_station_title, number));
+        if (actionBarToolbar == null) {
+            actionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+            if (actionBarToolbar != null) {
+                setSupportActionBar(actionBarToolbar);
+            }
         }
     }
 }
