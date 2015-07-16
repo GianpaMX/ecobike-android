3c4c21b6a0aa9d87b2770727ef3c1b1ca39dbb06
Juan Villa <juan64@gmail.com>
2015-07-15 22:56:26 -0500

    Adding a backdrop map

--- app/src/main/java/mx/softux/ecobike/activities/StationActivity.java
+++ app/src/main/java/mx/softux/ecobike/activities/StationActivity.java
@@ -7,6 +7,13 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import mx.softux.ecobike.P;
 import mx.softux.ecobike.R;
 import mx.softux.ecobike.library.Station;
@@ -14,7 +21,7 @@ import mx.softux.ecobike.library.Station;
 /**
  * Created by juan on 5/21/15.
  */
-public class StationActivity extends AppCompatActivity {
+public class StationActivity extends AppCompatActivity implements OnMapReadyCallback {
     private Toolbar actionBarToolbar;
     private Station station = new Station();
 
@@ -29,13 +36,24 @@ public class StationActivity extends AppCompatActivity {
         }
         CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
 
-        RecyclerView stationListRecyclerView = (RecyclerView)findViewById(R.id.station_recycler_view);
+        RecyclerView stationListRecyclerView = (RecyclerView) findViewById(R.id.station_recycler_view);
         stationListRecyclerView.setLayoutManager(new LinearLayoutManager(stationListRecyclerView.getContext()));
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.backdrop_map);
+        mapFragment.getMapAsync(this);
 
         station.number = getIntent().getIntExtra(P.Station.NUMBER, 0);
 
         String title = getString(R.string.station_activity_title, station.number);
         setTitle(title);
         collapsingToolbar.setTitle(title);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng home = new LatLng(19.409, -99.188);
+        googleMap.addMarker(new MarkerOptions().position(home).title("Home"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(home));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(10));
     }
 }
